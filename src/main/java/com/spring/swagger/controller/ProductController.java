ac3d8168b7258b0b5960b1bc24b9b1022e3879eb
dizsart <darogadibia@gmail.com>
2020-06-19 21:15:10 +0100

    add the services to the project structure

--- src/main/java/com/spring/swagger/controller/ProductController.java
+++ src/main/java/com/spring/swagger/controller/ProductController.java
@@ -1,4 +1,16 @@
 package com.spring.swagger.controller;
 
+import com.spring.swagger.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+    private ProductService productService;
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 }
