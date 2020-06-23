6a4a3de139b4afc2398f4760adb94d19e8c83d21
dizsart <darogadibia@gmail.com>
2020-06-23 13:30:20 +0100

    add the iterable product list method to the product controller class of the project structure

--- src/main/java/com/spring/swagger/controller/ProductController.java
+++ src/main/java/com/spring/swagger/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.spring.swagger.controller;
 
 import com.spring.swagger.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +15,11 @@ public class ProductController {
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
+
+    @RequestMapping(value = "/list", method= RequestMethod.GET)
+    public Iterable list(Model model){
+        Iterable productList = productService.listAllProducts();
+        return productList;
+    }
+
 }
