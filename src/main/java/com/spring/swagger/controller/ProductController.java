ab93a89cd8f798afb5c9a63b079123a4405c3bd2
dizsart <darogadibia@gmail.com>
2020-06-23 13:33:16 +0100

    add the show product method to the controller class of the project structure

--- src/main/java/com/spring/swagger/controller/ProductController.java
+++ src/main/java/com/spring/swagger/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.spring.swagger.controller;
 
+import com.spring.swagger.domain.Product;
 import com.spring.swagger.service.ProductService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +25,10 @@ public class ProductController {
         return productList;
     }
 
+    @ApiOperation(value = "Search a product with an ID",response = Product.class)
+    @RequestMapping(value = "/show/{id}", method= RequestMethod.GET, produces = "application/json")
+    public Product showProduct(@PathVariable Integer id, Model model){
+        Product product = productService.getProductById(id);
+        return product;
+    }
 }
