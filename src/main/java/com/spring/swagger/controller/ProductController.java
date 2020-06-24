af1d71d511c8be67199f6120c08f89e27ea9ce54
dizsart <darogadibia@gmail.com>
2020-06-24 21:24:29 +0100

    make methods in product service

--- src/main/java/com/spring/swagger/controller/ProductController.java
+++ src/main/java/com/spring/swagger/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.spring.swagger.controller;
 
 import com.spring.swagger.domain.Product;
-import com.spring.swagger.service.ProductService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/com/spring/swagger/service/ProductService.java
+++ src/main/java/com/spring/swagger/service/ProductService.java
@@ -1,7 +1,14 @@
 package com.spring.swagger.service;
 
-import org.springframework.stereotype.Service;
 
-@Service
-public class ProductService {
+import com.spring.swagger.domain.Product;
+
+public interface ProductService {
+    Iterable<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveProduct(Product product);
+
+    void deleteProduct(Integer id);
 }
