8611f81fa174213e640029a54d7b5f2920cabd9a
dizsart <darogadibia@gmail.com>
2020-06-25 19:34:23 +0100

    make ProductServiceImpl class

--- src/main/java/com/spring/swagger/repository/ProductRepository.java
+++ src/main/java/com/spring/swagger/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.spring.swagger.repository;
 
 import com.spring.swagger.domain.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
 }
--- src/main/java/com/spring/swagger/service/ProductService.java
+++ src/main/java/com/spring/swagger/service/ProductService.java
@@ -5,7 +5,7 @@ import com.spring.swagger.domain.Product;
 import org.springframework.stereotype.Service;
 
 
-@Service
+
 public interface ProductService {
     Iterable<Product> listAllProducts();
 
