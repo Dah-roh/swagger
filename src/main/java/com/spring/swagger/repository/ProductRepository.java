4f51ff2aa462e2788ff6964a78424710c8a70cdb
dizsart <darogadibia@gmail.com>
2020-06-25 19:38:27 +0100

    make methods in productRepository

--- src/main/java/com/spring/swagger/repository/ProductRepository.java
+++ src/main/java/com/spring/swagger/repository/ProductRepository.java
@@ -5,7 +5,12 @@ import com.spring.swagger.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
+    Product deleteById(Integer id);
+
+    Optional<Product> findById(Integer id);
 }
